a70f702afd64a3c680c98d01c642e8dc323aa16e
Zachary Sweet <ztsc97@mst.edu>
2017-10-22 11:45:43 -0500

    Completed Build-A-Blog assignment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, session
+from flask import Flask, request, redirect, render_template, session,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -22,34 +22,57 @@ class Blog(db.Model):
 
 @app.route('/blog', methods=['POST','GET'])
 def blog():
-    
-    if request.method == 'POST':
+
+    blog = request.args.get('id')
+    if blog:
+        ind_blog = Blog.query.get(blog)
+        return render_template('individual_blog.html',ind_blog=ind_blog)
+
+    if request.method == "post":
         blog = request.form['blog']
         title = request.form['title']
-        if len(title) == 0 and len(blog) ==0:
-            blog_error = "Blog can't be left blank"
-            title_error = "Title can't be left blank"
-            return render_template('newpost.html',blog_error=blog_error,title_error=title_error)
-        elif len(title)== 0:
-            title_error = "Title can't be left blank"
-            return render_template('newpost.html',title_error=title_error)
-        elif len(blog) == 0:
-            blog_error = "Blog can't be left blank"
-            return render_template('newpost.html',blog_error=blog_error)
-        else:
-            new_blog = Blog(blog,title)
-            db.session.add(new_blog)
-            db.session.commit()
+        new_blog = Blog(blog,title)
+        db.session.add(new_blog)
+        db.session.commit()
+        new_direct = "/blog?id=" + str(new_blog.id)
+        
+        return redirect( new_direct)
+    else:
+        blogs= Blog.query.all()
 
-    blogs= Blog.query.all()
-    titles = Blog.query.all()
-    
-    return render_template('blog.html',blogs=blogs,titles=titles)
+
+        return render_template('blog.html',blogs=blogs)
 
 @app.route('/newpost', methods=['POST','GET'])
 def newpost():
+    blog = request.args.get('id')
+    if blog:
+        ind_blog = Blog.query.get(blog)
+        return render_template('individual_blog.html',ind_blog=ind_blog)
+  
+    if request.method == 'POST':
+        blog = request.form['blog']
+        title = request.form['title']
+        if len(title)!=0 and len(blog) != 0:
+            new_blog = Blog(blog,title)
+            db.session.add(new_blog)
+            db.session.commit()
+            new_direct = "/blog?id=" + str(new_blog.id)
+            return redirect(new_direct)
+        if len(title) == 0 and len(blog) ==0:
+            blog_error = "Blog can't be left blank"
+            title_error = "Title can't be left blank"
+            return render_template("newpost.html",blog_error=blog_error,title_error=title_error)
+        elif len(title)== 0 and len(blog) != 0:
+            title_error = "Title can't be left blank"
+            blog = request.form['blog']
+            return render_template("newpost.html",blog=blog,title_error=title_error)
+        elif len(blog) == 0 and len(title)!=0:
+            blog_error = "Blog can't be left blank"
+            title = request.form['title']
+            return render_template('newpost.html',blog_error=blog_error,title=title)
+    else:
+        return render_template('newpost.html')
     
-    return render_template('newpost.html',**locals())
-
 if __name__ == '__main__':
     app.run()
